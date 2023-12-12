a3406c35c9d45ac90900d75ccf46dd7233288c34
Shebeli <Shervinsaeedi78@gmail.com>
2023-12-12 03:34:58 +0330

    Added an incomplete dropdown list to input field. Selecting one of the options from the drop down list will call fetchCityWeatherData.

--- frontend/weatherapp/src/App.vue
+++ frontend/weatherapp/src/App.vue
@@ -1,30 +1,37 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { onKeyStroke } from '@vueuse/core'
 
 const cityName = ref('')
 const cityWeatherData = ref('')
 const isLoading = ref(false)
 const similarCityNames = ref([])
+const selectedCity = ref('')
 let debounceTimer = null
 
 onKeyStroke('Enter', () => {
   fetchCityWeatherData(cityName)
 })
 
-async function delayedCityNamesFetch() {
-  clearTimeout(debounceTimer)
+async function fetchSimilarCityNames() {
+  clearTimeout(debounceTimer);
+  let responseData = [];
   debounceTimer = setTimeout(async () => {
     try {
       const response = await fetch(
         `http://127.0.0.1:8000/api/city-names?city=${cityName.value}`
       );
-    if (response.status !== 200) {
-      return;
-    }
-  } catch (error) {
-    cityWeatherData.value = error;
-  }
+      responseData = await response.json();
+      if (response.status !== 200) {
+        return;
+      };
+    } 
+    catch (error) {
+      cityWeatherData.value = error;
+    };
+    responseData.forEach(cityName => {
+      similarCityNames.value.push(cityName[0]);
+    });
   }, 1000);
 }
 
@@ -33,7 +40,7 @@ async function fetchCityWeatherData() {
   cityWeatherData.value = null;
   try {
     const response = await fetch(
-      `http://127.0.0.1:8000/weather/?city=${cityName.value}`
+      `http://127.0.0.1:8000/api/current-weather?city=${cityName.value}`
     );
     cityWeatherData.value = await response.json();
     if (response.status !== 200) {
@@ -46,6 +53,15 @@ async function fetchCityWeatherData() {
   }
 }
 
+
+onMounted(() => {
+  const inputField = document.getElementById("cityNameInput")
+  inputField.addEventListener('input', fetchSimilarCityNames)
+})
+
+function onCitySelect() {
+  fetchCityWeatherData(selectedCity.value)
+}
 </script>
 
 <style>
@@ -57,8 +73,21 @@ async function fetchCityWeatherData() {
   height: 50px;
   animation: spin 1s linear infinite;
 }
+.search-container {
+  position: relative;
+}
 
-
+.dropdown {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  max-height: 150px;
+  overflow-y: auto;
+  z-index: 1;
+}
 
 @keyframes spin {
   0% {
@@ -69,19 +98,27 @@ async function fetchCityWeatherData() {
     transform: rotate(360deg);
   }
 }
+
 </style>
 
 
 <template>
   <h2>
     Enter the city name
-    <p>
-    <input v-model="cityName" id="cityInput" @keyup.enter="fetchCityWeatherData(cityName)">
+    <div class="search-container">  
+      <input v-model="cityName" id="cityNameInput" @keyup.enter="fetchCityWeatherData(cityName)">
       <button @click="fetchCityWeatherData(cityName)" :disabled="cityWeatherData == null"> Search</button>
-    </p>
+      <div v-if="similarCityNames.length" class="dropdown">
+        <select v-model="selectedCity" @change="onCitySelect">
+          <option v-for="city in similarCityNames" :key="city" :value="city">{{ city }}</option>
+        </select>
+      </div>
+    
+    </div>
     <p v-if="isLoading" class="loading-spinner"></p>
     <pre v-else-if="cityWeatherData">{{ cityWeatherData }}</pre>
     <p v-else></p>
+    
   </h2>
 </template>
 
