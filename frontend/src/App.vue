db384687af30102fae617f4d5249c6cb8bd4fbc9
Shervin Saeedi Borna <Shervinsaeedi78@gmail.com>
2023-12-20 01:39:32 +0100

    Made early dockerfiles and compose.yaml files. Changed the directory of frontend weather app content from frontend/weatherapp/ to frontend/ Added a new getWeatherIconPath function with the associated objct for returning the correct weather icon path and updated the HTML tag for the associated value.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,67 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { onKeyStroke } from '@vueuse/core'
+import weatherConditionIconIds from '@/utils/weatherIcons.js'
 
 const cityName = ref('')
 const cityWeatherData = ref('')
 const isLoading = ref(false)
 const similarCityNames = ref([])
 const selectedCity = ref('')
-const weatherIcon = ref(null)
+const weatherIcon = ref('')
 
 let debounceTimer = null
-// const cityWeatherConditionIds = {
-//   'Clear' : [800],
-//   'Clouds': [801, 802, 803, 804],
-//   '
-// }
+
 
 onKeyStroke('Enter', () => {
   fetchCityWeatherData(cityName)
 })
 
+function getWeatherIconPath(weatherCode) {
+  const baseFilePath = '/src/assets/static/';
+  const weatherIconFileName = weatherConditionIconIds[weatherCode].iconName;
+  return baseFilePath + weatherIconFileName
+}
+
 async function fetchSimilarCityNames() {
-  clearTimeout(debounceTimer);
-  let responseData = [];
-  similarCityNames.value = [];
+  clearTimeout(debounceTimer)
+  let responseData = []
+  similarCityNames.value = []
   debounceTimer = setTimeout(async () => {
     try {
-      const response = await fetch(
-        `http://127.0.0.1:8000/api/city-names?city=${cityName.value}`
-      );
-      responseData = await response.json();
+      const response = await fetch(`http://127.0.0.1:8000/api/city-names?city=${cityName.value}`)
+      responseData = await response.json()
       if (response.status !== 200) {
-        return;
-      };
-    } 
-    catch (error) {
-      cityWeatherData.value = error;
-    };
-    responseData.forEach(cityName => {
-      similarCityNames.value.push(cityName['name']);
-    });
-  }, 400);
+        return
+      }
+    } catch (error) {
+      cityWeatherData.value = error
+    }
+    responseData.forEach((cityName) => {
+      similarCityNames.value.push(cityName['name'])
+    })
+  }, 400)
 }
 
 async function fetchCityWeatherData() {
-  isLoading.value = true;
-  cityWeatherData.value = null;
+  isLoading.value = true
+  cityWeatherData.value = null
   try {
-    const response = await fetch(
-      `http://127.0.0.1:8000/api/current-weather?city=${cityName.value}`
-    );
-    cityWeatherData.value = await response.json();
+    const response = await fetch(`http://127.0.0.1:8000/api/current-weather?city=${cityName.value}`)
+    cityWeatherData.value = await response.json()
     if (response.status !== 200) {
-      return;
+      return
     }
   } catch (error) {
-    cityWeatherData.value = error;
+    cityWeatherData.value = error
   } finally {
-    isLoading.value = false;
-    const weatherCondition = cityWeatherData.value.cityWeatherCondition;
-    switch (weatherCondition) {
-      case 'Thunderstorm':
-        break
-      case 'Drizzle':
-        break
-      case 'Rain':
-        break
-      case 'Snow':
-        break
-      case 'Clear':
-        break
-      case 'Clouds':
-        break
-     
-    }
+    isLoading.value = false
+    weatherIcon.value = getWeatherIconPath(cityWeatherData.value.cityWeatherCondition.icon)
   }
 }
 
-
 onMounted(() => {
-  const inputField = document.getElementById("cityNameInput")
+  const inputField = document.getElementById('cityNameInput')
   inputField.addEventListener('input', fetchSimilarCityNames)
 })
 
@@ -107,14 +89,13 @@ function fetchSelectedCityWeatherData() {
   border-radius: 12px;
   border: 10px solid #1e065b;
   padding: 20px;
-  margin: 10px 10px; 
+  margin: 10px 10px;
   /* left: 50%; */
   bottom: 10%;
-  
 }
 
 .weather-container {
-  position:relative;
+  position: relative;
   background-color: #0f1041;
   border-radius: 12px;
   border: 10px solid #1e065b;
@@ -138,7 +119,7 @@ function fetchSelectedCityWeatherData() {
   border-radius: 10px;
   font-weight: 500;
 }
-.city-element:hover{
+.city-element:hover {
   color: aliceblue;
   background-color: #2f37af;
   cursor: pointer;
@@ -150,21 +131,21 @@ function fetchSelectedCityWeatherData() {
 
 .search-button {
   align-self: right;
-  background-image: linear-gradient(144deg,#ae44fa, #6851ff 50%,#0a043c);
+  background-image: linear-gradient(144deg, #ae44fa, #6851ff 50%, #0a043c);
   border-radius: 10px;
   box-shadow: rgba(154, 74, 245, 0.2) 0 15px 30px -5px;
-  color: #FFFFFF;
+  color: #ffffff;
   font-size: 18px;
   justify-content: center;
   padding: 10px 30px;
   cursor: pointer;
-  right: 20px; 
+  right: 20px;
   top: 60px;
   font-weight: 600;
 }
 
 .search-button:hover {
-  outline:#000000;
+  outline: #000000;
   color: aliceblue;
   background-color: #2f37af;
   cursor: pointer;
@@ -189,11 +170,11 @@ function fetchSelectedCityWeatherData() {
 #cityNameInput::placeholder {
   color: #888787;
   font-weight: 200;
-  background-image: url("./assets/static/search_icon.jpg");
+  background-image: url('./assets/static/search_icon.jpg');
   /* background-color: azure; */
   background-size: 27px;
   background-repeat: no-repeat;
-  background-position: -4px ;
+  background-position: -4px;
 }
 
 #weatherIcon {
@@ -202,7 +183,6 @@ function fetchSelectedCityWeatherData() {
   background-color: none;
   color: none;
 }
-
 
 @keyframes spin {
   0% {
@@ -213,32 +193,50 @@ function fetchSelectedCityWeatherData() {
     transform: rotate(360deg);
   }
 }
-
 </style>
 
-
 <template>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@500&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@500&display=swap"
+    rel="stylesheet"
+  />
   <div class="search-container">
-    <div class="city-search"> 
-      <input placeholder="    City Name" v-model="cityName" id="cityNameInput" @keyup.enter="fetchCityWeatherData(cityName)">
-      <button class="search-button" @click="fetchCityWeatherData(cityName)" :disabled="cityWeatherData == null"> Search</button>
+    <div class="city-search">
+      <input
+        placeholder="    City Name"
+        v-model="cityName"
+        id="cityNameInput"
+        @keyup.enter="fetchCityWeatherData(cityName)"
+      />
+      <button
+        class="search-button"
+        @click="fetchCityWeatherData(cityName)"
+        :disabled="cityWeatherData == null"
+      >
+        Search
+      </button>
     </div>
     <div v-if="similarCityNames.length" class="dropdown">
       <ul class="city-list">
-        <li class="city-element" v-for="city in similarCityNames" :key="city" :value="city" @click="fetchSelectedCityWeatherData">{{ city }}</li>
+        <li
+          class="city-element"
+          v-for="city in similarCityNames"
+          :key="city"
+          :value="city"
+          @click="fetchSelectedCityWeatherData"
+        >
+          {{ city }}
+        </li>
       </ul>
     </div>
   </div>
   <div class="weather-container" v-if="cityWeatherData">
-    <img :src='importedSVG' id='weatherIcon'/>
+    <img :src="weatherIcon" id="weatherIcon" />
     <span>{{ cityWeatherData }}</span>
   </div>
   <p v-else-if="isLoading" class="loading-spinner"></p>
 
   <p v-else></p>
-
 </template>
-
