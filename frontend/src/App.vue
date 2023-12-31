462771761756a69bbc7ac6491ecd43bfd07ac61d
Shervin Saeedi Borna <Shervinsaeedi78@gmail.com>
2024-01-01 00:46:41 +0100

    Frontend: Moved googles font api link tags to index.html file. Updated vite config to proceed ion icons as custom elements. Seperated footer into a different vue component. Added a new vue icon component for 404 requests. Refactored fetchCityWeatherData to also handle 404 requests. Updated CSS.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,13 @@ import { ref, onMounted, defineAsyncComponent, shallowRef } from 'vue'
 import { onKeyStroke } from '@vueuse/core'
 
 import WeatherConditions from './utils/WeatherIcons.vue'
-import WeatherAdditionalData from './components/WeatherAdditionalData.vue';
+import IconNotFound from './components/icons/IconNotFound.vue'
+import WeatherAdditionalData from './components/WeatherAdditionalData.vue'
+import PageFooter from './components/PageFooter.vue'
 
-
-const cityName = ref('')
+const selectedCityName = ref('')
 const cityWeatherData = ref('')
 const similarCityNames = ref([])
-const selectedCity = ref('')
 const weatherIcon = shallowRef('')
 
 const isLoading = ref(false)
@@ -18,7 +18,7 @@ const showNotification = ref(false)
 let debounceTimer = null
 
 onKeyStroke('Enter', () => {
-  fetchCityWeatherData(cityName)
+  fetchCityWeatherData(selectedCityName)
 })
 
 function getWeatherIconPath(weatherCode) {
@@ -27,8 +27,8 @@ function getWeatherIconPath(weatherCode) {
 }
 
 const closeNotification = () => {
-  showNotification.value = false;
-  localStorage.setItem('notificationClose', 'true');
+  showNotification.value = false
+  localStorage.setItem('notificationClose', 'true')
 }
 
 async function fetchSimilarCityNames() {
@@ -37,7 +37,9 @@ async function fetchSimilarCityNames() {
   similarCityNames.value = []
   debounceTimer = setTimeout(async () => {
     try {
-      const response = await fetch(`http://127.0.0.1:8000/api/city-names?city=${cityName.value}`)
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/city-names?city=${selectedCityName.value}`
+      )
       responseData = await response.json()
       if (response.status !== 200) {
         return
@@ -45,40 +47,47 @@ async function fetchSimilarCityNames() {
     } catch (error) {
       cityWeatherData.value = error
     }
+    similarCityNames.value = []
     responseData.forEach((cityName) => {
       similarCityNames.value.push(cityName['name'])
     })
-  }, 400)
+  }, 600)
 }
 
+
 async function fetchCityWeatherData() {
-  isLoading.value = true
-  cityWeatherData.value = null
-  try {
-    const response = await fetch(`http://127.0.0.1:8000/api/current-weather?city=${cityName.value}`)
-    cityWeatherData.value = await response.json()
-    if (response.status !== 200) {
-      return
-    }
-  } catch (error) {
-    cityWeatherData.value = error
-  } finally {
-    isLoading.value = false
-    const iconFile = getWeatherIconPath(cityWeatherData.value.cityweathercondition.icon)
-    weatherIcon.value = iconFile
+  isLoading.value = true;
+  cityWeatherData.value = null;
+  const response = await fetch(
+      `http://127.0.0.1:8000/api/current-weather?city=${selectedCityName.value}`
+    );
+  if (response.status == 200) {   
+    cityWeatherData.value = await response.json();  
+    const iconFile = getWeatherIconPath(cityWeatherData.value.cityweathercondition.icon);
+    weatherIcon.value = iconFile;
   }
+  else if (response.status == 404) {
+    cityWeatherData.value = 404;
+  }
+  else {
+    console.error(response)
+  }
+  isLoading.value = false;
+  similarCityNames.value = '';
 }
 
 onMounted(() => {
-  const inputField = document.getElementById('cityNameInput')
-  inputField.addEventListener('input', fetchSimilarCityNames)
+  const inputField = document.getElementById('cityNameInput');
+  inputField.addEventListener('input', fetchSimilarCityNames);
   if (!localStorage.getItem('notificationClose')) {
-    showNotification.value = true
+    showNotification.value = true;
   }
 })
 
-function fetchSelectedCityWeatherData() {
-  fetchCityWeatherData(selectedCity.value)
+function fetchSelectedCityWeatherData(city) {
+  similarCityNames.value = ''
+  selectedCityName.value = city
+  fetchCityWeatherData()
 }
 </script>
 
@@ -87,18 +96,12 @@ function fetchSelectedCityWeatherData() {
 </style>
 
 <template>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@500&display=swap"
-    rel="stylesheet"
-  />
   <div v-if="showNotification" class="overlay"></div>
   <div v-if="showNotification" class="notification">
+    <p>Welcome to my first application made with Vue3.</p>
     <p>
-      Welcome to my first application made with Vue3.  
-    </p> 
-    <p>If you have any suggestions or feedback for this app, you can contact me through my socials.</p>
+      If you have any suggestions or feedback for this app, you can contact me through my socials.
+    </p>
     <p>Also if you like this simple app, please consider leaving a star on the github page 😉</p>
     <ion-icon name="close-circle-sharp" @click="closeNotification"></ion-icon>
   </div>
@@ -107,13 +110,13 @@ function fetchSelectedCityWeatherData() {
       <div class="city-search">
         <input
           placeholder="    City Name"
-          v-model="cityName"
+          v-model="selectedCityName"
           id="cityNameInput"
-          @keyup.enter="fetchCityWeatherData(cityName)"
+          @keyup.enter="fetchCityWeatherData(selectedCityName)"
         />
         <button
           class="search-button"
-          @click="fetchCityWeatherData(cityName)"
+          @click="fetchCityWeatherData(selectedCityName)"
           :disabled="cityWeatherData == null"
         >
           Search
@@ -126,15 +129,19 @@ function fetchSelectedCityWeatherData() {
             v-for="city in similarCityNames"
             :key="city"
             :value="city"
-            @click="fetchSelectedCityWeatherData"
+            @click="fetchSelectedCityWeatherData(city)"
           >
             {{ city }}
           </li>
         </ul>
       </div>
     </div>
-    <div class="weather-container" v-if="cityWeatherData">
-      <weatherIcon id="weatherIcon"/>
+    <div class="city-not-found" v-if="cityWeatherData == '404'">
+      <h2>Given city name was not found!</h2>
+      <IconNotFound/>
+    </div>
+    <div class="weather-container" v-else-if="cityWeatherData">
+      <weatherIcon id="weatherIcon" />
       <div class="weather-main">
         <h1>{{ cityWeatherData.name }}</h1>
         <h1>{{ Math.round(cityWeatherData.cityweather.temp) }}°C</h1>
@@ -144,19 +151,9 @@ function fetchSelectedCityWeatherData() {
         </p>
         <p>Feels like: {{ Math.round(cityWeatherData.cityweather.feels_like) }}°C</p>
       </div>
-      <WeatherAdditionalData :weatherData="cityWeatherData"/>
+      <WeatherAdditionalData :weatherData="cityWeatherData" />
     </div>
     <p v-else-if="isLoading" class="loading-spinner"></p>
   </div>
-  <footer>
-      <h3>Made by Shebeli</h3>
-      <h2>
-        Made with:
-      </h2>
-      <span>
-        <ion-icon name="logo-python"></ion-icon>
-        <ion-icon name="logo-vue"></ion-icon>
-        <ion-icon name="logo-css3"></ion-icon>
-      </span>
-    </footer>
+  <PageFooter />
 </template>
---
+++ frontend/src/components/PageFooter.vue
@@ -0,0 +1,42 @@
+<template>
+    <footer>
+        <h2>Made by Shebeli</h2>
+        <span>
+        <ion-icon name="logo-python"></ion-icon>
+        <ion-icon name="logo-vue"></ion-icon>
+        <ion-icon name="logo-css3"></ion-icon>
+        </span>
+        <span id="socials">
+        <a href="https://discordapp.com/users/335818111485870081">
+            <ion-icon name="logo-discord"></ion-icon>
+        </a>
+        <a href="https://github.com/Shebeli/WeatherApp">
+            <ion-icon name="logo-github"></ion-icon>
+        </a>
+        <a href="https://wa.link/qe2wue">
+            <ion-icon name="logo-whatsapp"></ion-icon>
+        </a>
+        </span>
+    </footer>
+</template>
+
+<style>
+footer {
+  position: fixed;
+  background-color: #111010;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  height: 100px;
+  margin-top: auto;
+  color: white;
+  /* display: block; */
+  /* min-height: 10%; */
+}
+
+footer span {
+  font-size: 28px;
+  color: white;
+}
+
+</style>
